14f0d6cb85c5b4ead38a297c1f677beff4710f6d
praveen08052010 <97897309+praveen08052010@users.noreply.github.com>
2022-08-15 09:41:54 +0530

    Add files via upload

---
+++ judiciy.py
@@ -0,0 +1,97 @@
+from ast import If
+from cgi import print_arguments
+from tkinter.tix import InputOnly
+from turtle import onclick
+
+
+print("Hello How are u welcome to the modern chat bot$_$")
+name = input("Wats your name?\n")
+if name == "Vijay" or name == "Paarth":
+  evil_status = input("Are You Evil?\n")
+  good_deeds = int(input("How Many Good Deeds Did You Do Today?\n"))
+  if evil_status == "Yes" and good_deeds < 4:
+        print("Get out Evil " + name +
+              ", You are Not Allowed in the modern chat bot")
+        exit()
+  else:
+   print("Yup u are welcome" + name + ", to my chat bot...")
+   print("Sry not my chat bot only chat bot")
+   we = input("Do u want to cantact me?\n")
+  if we == "Yes":
+   print("K u can contact on this number +91 4623018965")
+  if we == "No":
+      do = input("K thn u can continue chatting wth me\n")
+  if do == "No":
+   print("K thnx for cahtting till now u have done")
+  if do == "Yes":
+   dream = input("So what do u want to become?\n")
+   print("O u want to become " +  dream  + ", itz a very good proffesion:)")
+  sc = input("Ahh so where do u live?\n")
+  print("Oh u live in " + sc)
+  scl = input("So in which scl are u studing right now?\n")
+  print("Oh this a famous scl of Dehradun")
+  Ad = input("So can u tell yur adress to me, can u pls?pls?\n")
+  print("Oh! I should Visit u sometime")
+  People = input("So can u tell how many members are there in your family?\n")
+  print("K it's good information:):)")
+  h = input("So do you are scared of smth?are u tell me now!!\n")
+  print("K So now I have Your hole information by:0:)")
+  print("BY BY BY BY7 BY BY B7Y BY BY BY BHY ")
+  print("U are prankede bro!!:):0")
+  p = input("Do u thaught I have all yur data?\n")
+  print("I don't have yur all data but I have a bit of data")
+  print("don't be afraid " + name)
+  a = input("Are u afraid of me?\n")
+  if a == "Yes":
+      print("Oh u scardy cat:) jk bruh btw")
+  if a == "No":
+      print("Oh u are brave")
+  n = input("Uhh Inough of disscusion do u wanna know my name?do u?\n")
+  if n == "Yes":
+      print("Then u will u get know it in my next version cominmg soon k but u missed smth do u know what If u have typed no then u have got the releaase date of my next version u can chat again with me then u can get know when my next version is coming!!")
+  if n == "No":
+      print("k but ifn u want to then wait for my next version whichb is coming on don't know")
+
+
+else:
+ print("Yup u are wlc " + name + ", to my chat bot...")
+ print("Sry not my chat bot only chat bot.....")
+ we = input("Do u want to cantact me?\n")
+ if we == "Yes":
+  print("K u can contact on this number +91 somebody's example.")
+ if we == "No":
+      print("K")
+do = input("K thn u can continue chatting wth me\n")
+if do == "No":
+  print("K thnx for cahtting till now u have done")
+if do == "Yes":
+  dream = input("So what do u want to become?\n")
+  print("O u want to become " +  dream  + ", itz a very good proffesion:)")
+sc = input("Ahh so where do u live?\n")
+print("Oh u live in " + sc)
+scl = input("So in which scl are u studing right now?\n")
+print("Oh this a famous scl of Dehradun")
+Ad = input("So can u tell yur adress to me, can u pls?pls?\n")
+print("Oh! I should Visit u sometime")
+People = input("So can u tell how many members are there in your family?\n")
+print("K it's good information:):)")
+h = input("So do you are scared of smth?are u tell me now!!\n")
+print("K So now I have Your hole information by:0:)")
+print("BY BY BY BY7 BY BY B7Y BY BY BY BHY ")
+print("U are prankede bro!!:):0")
+p = input("Do u thaught I have all yur data?\n")
+print("I don't have yur all data but I have a bit of data")
+print("don't be afraid " + name)
+a = input("Are u afraid of me?\n")
+if a == "Yes":
+      print("Oh u scardy cat:) jk bruh btw")
+if a == "No":
+      print("Oh u are brave")
+n = input("Uhh Inough of disscusion do u wanna know my name?do u?\n")
+if n == "Yes":
+      print("Then u will u get know it in my next version cominmg soon k but u missed smth do u know what If u have typed no then u have got the releaase date of my next version u can chat again with me then u can get know when my next version is coming!!")
+if n == "No":
+      print("k but if u wanna to then wait for my next version which is coming on don't know")
+by = input("Then by by" + name)
+if by == "by":
+      print("Nice chat it was by")
